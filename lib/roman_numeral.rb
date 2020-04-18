0189fcadf6c5ab04dc2e8f983daeed673420b791
Catriona <catrionamairi@blueyonder.co.uk>
2020-04-18 17:06:02 +0100

    Tests and code added for numbers 7-10

--- lib/roman_numeral.rb
+++ lib/roman_numeral.rb
@@ -1,11 +1,15 @@
 class RomanNumeral
   def convert(n)
     case
-    when n == 0 then ""
-    when n < 4 then "I" * n
-    when n == 4 then "IV"
-    when n == 5 then "V"
-    else "VI"
+      when n == 0 then ""
+      when n < 4 then "I" * n
+      when n == 4 then "IV"
+      when n == 5 then "V"
+      when n == 6 then "VI"
+      when n == 7 then "VII"
+      when n == 8 then "VIII"
+      when n == 9 then "IX"
+      else "X"
     end
   end
 end
--- spec/roman_numeral_spec.rb
+++ spec/roman_numeral_spec.rb
@@ -28,4 +28,20 @@ describe 'convert to Roman numerals' do
   it 'returns "VI" when passed 6' do
     expect(RomanNumeral.new.convert(6)).to eq "VI"
   end
+
+  it 'returns "VII" when passed 7' do
+    expect(RomanNumeral.new.convert(7)).to eq "VII"
+  end
+
+  it 'returns "VIII" when passed 8' do
+    expect(RomanNumeral.new.convert(8)).to eq "VIII"
+  end
+
+  it 'returns "IX" when passed 9' do
+    expect(RomanNumeral.new.convert(9)).to eq "IX"
+  end
+
+  it 'returns "X" when passed 10' do
+    expect(RomanNumeral.new.convert(10)).to eq "X"
+  end
 end
